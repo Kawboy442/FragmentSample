88118499932a1f5e7018b421ce4fdf8ed008e4da
R.Ito <kawboy442@gmail.com>
2020-02-18 21:51:05 +0900

    feat: edit onCreateView method

--- app/src/main/java/com/websavra/wings/android/fragmentsample/MenuListFragment.kt
+++ app/src/main/java/com/websavra/wings/android/fragmentsample/MenuListFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +18,19 @@ class MenuListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_menu_list, container, false)
+        val lvMenu = view.findViewById<ListView>(R.id.lvMenu)
+
+        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
+
+        val from = arrayOf("name", "price")
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        val adapter = SimpleAdapter(activity, menuList, android.R.layout.simple_list_item_2
+        , from, to)
+        lvMenu.adapter = adapter
+
+        return view
     }
 
 
