af20352031b4efab4394d7a10859e806604b5ca7
R.Ito <kawboy442@gmail.com>
2020-02-19 21:14:23 +0900

    feat: edit MenuThanksFragment

--- app/src/main/java/com/websavra/wings/android/fragmentsample/MenuThanksFragment.kt
+++ app/src/main/java/com/websavra/wings/android/fragmentsample/MenuThanksFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
@@ -16,9 +18,30 @@ class MenuThanksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_thanks, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_menu_thanks, container, false)
+
+        val intent = activity?.intent
+        val extras = intent?.extras
+
+        val menuName = extras?.getString("menuName")
+        val menuPrice = extras?.getString("menuPrice")
+
+        val tvMenuName = view.findViewById<TextView>(R.id.tvMenuName)
+        val tvMenuPrice = view.findViewById<TextView>(R.id.tvMenuPrice)
+
+        tvMenuName.text = menuName
+        tvMenuPrice.text = menuPrice
+
+        val btBackButton = view.findViewById<Button>(R.id.btBackButton)
+        btBackButton.setOnClickListener(ButtonClickListener)
+
+        return view
     }
 
-
+    private inner class ButtonClickListener: View.OnClickListener {
+        override fun onClick(v: View?) {
+            activity?.finish()
+        }
+    }
 }
